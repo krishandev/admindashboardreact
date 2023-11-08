0782695beef158c6ce15429f1c56235f38b22522
Krishan <krishandev.0102@gmail.com>
2023-11-09 00:48:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+import Loader from './components/Loader'
+
+
+const Dashboard=lazy(()=>import("./pages/Dashboard"))
+const Products=lazy(()=>import('./pages/Products'))
+const Transaction=lazy(()=>import('./pages/Transaction'))
+const Customers=lazy(()=>import('./pages/Customers'))
+const NewProduct=lazy(()=>import('./pages/management/NewProduct'))
+const ProductManagement=lazy(()=>import('./pages/management/ProductManagement'))
+const TransactionManagement=lazy(()=>import('./pages/management/TransactionManagement'))
+const BarCharts=lazy(()=>import('./pages/charts/BarCharts'))
+const LineCharts=lazy(()=>import('./pages/charts/LineCharts'))
+const PieCharts=lazy(()=>import('./pages/charts/PieCharts'))
+const Coupon=lazy(()=>import('./pages/apps/Coupon'))
+const Stopwatch=lazy(()=>import('./pages/apps/Stopwatch'))
+const Toss=lazy(()=>import('./pages/apps/Toss'))
+
+const App = () => {
+  return (
+    <Router>
+    <Suspense fallback={<Loader/>}>
+    <Routes>
+      <Route path='/' element={<Link to="/admin/dashboard"><button className='btndashboard'>Visit Dashboard</button></Link>}/>
+        <Route path='/admin/dashboard' element={<Dashboard/>}/>
+        <Route path='/admin/products' element={<Products/>}/>
+        <Route path='/admin/transaction' element={<Transaction/>}/>
+        <Route path='/admin/customers' element={<Customers/>}/>
+ 
+      {/* Charts */}
+      <Route path='/admin/chart/bar' element={<BarCharts/>}/>
+      <Route path='/admin/chart/line' element={<LineCharts/>}/>
+      <Route path='/admin/chart/pie' element={<PieCharts/>}/>
+
+      {/* Apps */}
+      <Route path='/admin/app/coupon' element={<Coupon/>}/>
+      <Route path='/admin/app/stopwatch' element={<Stopwatch/>}/>
+      <Route path='/admin/app/toss' element={<Toss/>}/>
+
+        <Route path='/admin/product/new' element={<NewProduct/>}/>
+        <Route path='/admin/product/:id' element={<ProductManagement/>}/>
+        <Route path='/admin/transaction/:id' element={<TransactionManagement/>}/>
+        
+      </Routes>
+    </Suspense>
+    </Router>
+  )
+}
+
+export default App
